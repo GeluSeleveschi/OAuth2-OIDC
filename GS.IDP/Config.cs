862bad3b6d8b3f0727e4bed695bd81b5ccabcba5
gelu@codeindustry.net <gelu@codeindustry.net>
2023-04-29 21:41:19 +0300

    handled logout and authorization based on roles

--- GS.IDP/Config.cs
+++ GS.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace GS.IDP;
 
@@ -8,7 +9,8 @@ public static class Config
         new IdentityResource[]
         {
             new IdentityResources.OpenId(),
-            new IdentityResources.Profile()
+            new IdentityResources.Profile(),
+            new IdentityResource("roles", "Your role(s)", new [] {"role"})
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -17,5 +19,30 @@ public static class Config
 
     public static IEnumerable<Client> Clients =>
         new Client[]
-            { };
+            {
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "ImageGalleryClient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris =
+                    {
+                        "https://localhost:7184/signin-oidc"
+                    },PostLogoutRedirectUris =
+                    {
+                        "https://localhost:7184/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "roles"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    RequireConsent = true,
+                }
+            };
 }
