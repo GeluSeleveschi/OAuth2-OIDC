5b9d48a0478bd43a031b9a134c4ca5313a3e11b1
gelu@codeindustry.net <gelu@codeindustry.net>
2023-05-02 08:11:42 +0300

    added new authorization policies at API level

--- GS.IDP/Config.cs
+++ GS.IDP/Config.cs
@@ -16,16 +16,22 @@ public static class Config
 
     public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
     {
-        new ApiResource("imagegalleryapi", "Image Gallery API", new [] {"role"})
+        new ApiResource("imagegalleryapi", "Image Gallery API", new [] {"role","country"})
         {
-            Scopes = {"imagegalleryapi.fullaccess"}
+            Scopes = {
+                "imagegalleryapi.fullaccess",
+                "imagegalleryapi.read",
+                "imagegallery.write"
+            }
         }
     };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
             {
-                new ApiScope("imagegalleryapi.fullaccess")
+                new ApiScope("imagegalleryapi.fullaccess"),
+                new ApiScope("imagegalleryapi.read"),
+                new ApiScope("imagegalleryapi.write")
             };
 
     public static IEnumerable<Client> Clients =>
@@ -48,7 +54,9 @@ public static class Config
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
                         "roles",
-                        "imagegalleryapi.fullaccess",
+                        //"imagegalleryapi.fullaccess",
+                        "imagegalleryapi.read",
+                        "imagegalleryapi.write",
                         "country"
                     },
                     ClientSecrets =
